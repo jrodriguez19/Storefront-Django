271741a441020f12b4cd7531859c73cf939254eb
jrodriguez19 <jrodrigueznocua@gmail.com>
2023-10-02 17:54:03 +1100

    config files modified

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -160,10 +160,10 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
 
-DEFAULT_FROM_EMAIL = 'from@moshbuy.com'
+DEFAULT_FROM_EMAIL = 'from@georgebuy.com'
 
 ADMINS = [
-    ('Mosh', 'admin@moshbuy.com')
+    ('george', 'admin@georgebuy.com')
 ]
 
 CELERY_BEAT_SCHEDULE = {
